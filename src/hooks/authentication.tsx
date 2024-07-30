d633cc297491b5e7ee2ed91598219e18c7016048
LA4 <andr.ludow.com>
2024-07-30 17:50:54 +0200

    change api_trheadbok

--- src/hooks/authentication.tsx
+++ src/hooks/authentication.tsx
@@ -1,31 +1,31 @@
-
+const API_THREADBOOK = process.env.API_THREADBOOK;
 export default async function Authentication() {
-    const currentUser: string | undefined = document.cookie
-        .split("; ")
-        .find((row) => row.startsWith("access_token="))
-        ?.split("=")[1];
+  const currentUser: string | undefined = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("access_token="))
+    ?.split("=")[1];
 
-    if (!currentUser) {
-        return { result: false }
-    }
-    try {
-        const response = await fetch('http://localhost:3000/auth/login', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${currentUser}`
-            },
-        });
+  if (!currentUser) {
+    return { result: false };
+  }
+  try {
+    const response = await fetch(`${API_THREADBOOK}auth/login`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${currentUser}`,
+      },
+    });
 
-        const data = await response.json();
-        if (data.result) {
-            return data
-        }
-        if (data.statusCode === 401) {
-            return { result: false }
-        }
-    } catch (error) {
-        console.error("Erreur lors de la requête de connexion :", error);
-        return { result: false }
+    const data = await response.json();
+    if (data.result) {
+      return data;
     }
+    if (data.statusCode === 401) {
+      return { result: false };
+    }
+  } catch (error) {
+    console.error("Erreur lors de la requête de connexion :", error);
+    return { result: false };
+  }
 }
